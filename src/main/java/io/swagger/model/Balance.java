98edb88cdab35ac6287aff9c5e3b24b99f767f26
thijsvtol <43065692+thijsvtol@users.noreply.github.com>
2020-04-23 13:10:02 +0200

    heroku test

---
+++ src/main/java/io/swagger/model/Balance.java
@@ -0,0 +1,129 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * Balance
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-04-23T10:16:52.088Z[GMT]")
+public class Balance   {
+  @JsonProperty("id")
+  private Integer id = null;
+
+  @JsonProperty("accountId")
+  private Integer accountId = null;
+
+  @JsonProperty("balance")
+  private Double balance = null;
+
+  public Balance id(Integer id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Get id
+   * @return id
+  **/
+  @ApiModelProperty(example = "1", required = true, value = "")
+      @NotNull
+
+    public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public Balance accountId(Integer accountId) {
+    this.accountId = accountId;
+    return this;
+  }
+
+  /**
+   * Get accountId
+   * @return accountId
+  **/
+  @ApiModelProperty(example = "1", required = true, value = "")
+      @NotNull
+
+    public Integer getAccountId() {
+    return accountId;
+  }
+
+  public void setAccountId(Integer accountId) {
+    this.accountId = accountId;
+  }
+
+  public Balance balance(Double balance) {
+    this.balance = balance;
+    return this;
+  }
+
+  /**
+   * Get balance
+   * @return balance
+  **/
+  @ApiModelProperty(example = "250", required = true, value = "")
+      @NotNull
+
+    public Double getBalance() {
+    return balance;
+  }
+
+  public void setBalance(Double balance) {
+    this.balance = balance;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Balance balance = (Balance) o;
+    return Objects.equals(this.id, balance.id) &&
+        Objects.equals(this.accountId, balance.accountId) &&
+        Objects.equals(this.balance, balance.balance);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, accountId, balance);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Balance {\n");
+    
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    accountId: ").append(toIndentedString(accountId)).append("\n");
+    sb.append("    balance: ").append(toIndentedString(balance)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
