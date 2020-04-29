7bd74f8218a8f85906207ca6dc5ef902062a6485
Stef <stef.robbe@gmail.com>
2020-04-29 12:52:36 +0200

    push voor thijs

--- src/main/java/io/swagger/dao/TransactionRepository.java
+++ src/main/java/io/swagger/dao/TransactionRepository.java
@@ -1,15 +1,26 @@
 package io.swagger.dao;
 
+import io.swagger.model.Account;
 import io.swagger.model.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
 import java.util.List;
 
 @Repository
-public interface TransactionRepository extends CrudRepository<Transaction, Long> {
-
+public interface TransactionRepository extends PagingAndSortingRepository<Transaction, Long> {
     @Query("SELECT t FROM Transaction as t, Account a WHERE a.id = ?1")
-    Iterable<Transaction> getTransactionsByAccountId(int accountId);
+    Iterable<Transaction> getTransactionsByAccountId(long id);
+
+    // find all accounts with query values
+    Page<Transaction> findAll(Pageable pageable);
+
+    @Query("SELECT t FROM Transaction as t WHERE t.timestamp BETWEEN :dateFrom AND :dateTo")
+    Iterable<Transaction> findTransactionsByTimestampBetween(OffsetDateTime dateFrom, OffsetDateTime dateTo);
 }
--- src/main/java/io/swagger/service/TransactionService.java
+++ src/main/java/io/swagger/service/TransactionService.java
@@ -1,11 +1,13 @@
 package io.swagger.service;
 
-import io.swagger.dao.AccountRepository;
 import io.swagger.dao.TransactionRepository;
-import io.swagger.model.Account;
 import io.swagger.model.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -21,7 +23,17 @@ public class TransactionService {
         return (List<Transaction>) transactionRepository.findAll();
     }
 
-    public List<Transaction> getAllTransactionsByAccountId(int accountId) {
-        return (List<Transaction>) transactionRepository.getTransactionsByAccountId(accountId);
+    public List<Transaction> getAllTransactionsWithQuery(int offset, int limit) {
+        Pageable pageable = new PageRequest(offset,limit);
+        Page<Transaction> page = transactionRepository.findAll(pageable);
+        return page.getContent();
+    }
+
+    public List<Transaction> getAllTransactionsBetweenDates(LocalDate dateFrom, LocalDate dateTo) {
+        return (List<Transaction>) transactionRepository.findTransactionsByTimestampBetween(dateFrom, dateTo);
+    }
+
+    public List<Transaction> getAllTransactionsByAccountId(long id) {
+        return (List<Transaction>) transactionRepository.getTransactionsByAccountId(id);
     }
 }
