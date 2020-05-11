8e52fd37bfd74521ae013e3bc3d143456db44e47
thijsvtol <43065692+thijsvtol@users.noreply.github.com>
2020-05-11 17:36:29 +0200

    JS login

---
+++ src/main/resources/public/Script/login.js
@@ -0,0 +1,20 @@
+function Login(){
+    var username = document.getElementById("form-username").value;
+    var password = document.getElementById("form-password").value;
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/api/Login",
+        data: { username: username, password: password },
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        statusCode: {
+            200: function(result){
+                console.log("logged in" + result);
+                sessionStorage.setItem("session", result);
+                alert("[TEST] API Token saved as session: "+sessionStorage.getItem("session"))
+            }
+        }
+    });
+}
