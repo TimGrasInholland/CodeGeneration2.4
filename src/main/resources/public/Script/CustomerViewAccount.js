c632f000d5d6caf751008019937bbf1caf43ea55
Thijs van Tol <43065692+thijsvtol@users.noreply.github.com>
2020-05-13 11:57:44 +0200

    Update CustomerViewAccount.js

--- src/main/resources/public/Script/CustomerViewAccount.js
+++ src/main/resources/public/Script/CustomerViewAccount.js
@@ -22,26 +22,3 @@ function getTransaction(transaction) {
     date = `${da} ${mo} ${ye}`
     return '<div class="transaction-row"><p class="date-tag">'+date+'</p><p class="transaction-from-to">FROM: </p><p class="transaction-iban">'+transaction.accountFrom+'</p><p class="transaction-operator">+ </p><p class="transaction-amount">€ '+transaction.amount+'</p><p class="currency-tag">EUR</p><div class="hr-line"></div></div>';
 }
-
-
-
-
-
-
-// $.ajax({
-//     type: "GET",
-//     url: "http://localhost:8080/api/Users/2/Transactions",
-//     headers: {
-//         "session": "1"
-//     },
-//     statusCode: {
-//         200: handle200(data),
-//         201: handle201,
-//         404: handle404
-//     }
-// });
-
-// function handle200() {
-//     console.log("OK");
-    
-// }
