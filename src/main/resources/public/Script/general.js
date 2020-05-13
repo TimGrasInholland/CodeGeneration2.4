fb3a8f56ddc7380569c72dcb1a48f03c5286ec8f
thijsvtol <43065692+thijsvtol@users.noreply.github.com>
2020-05-13 13:02:33 +0200

    general css and js added

---
+++ src/main/resources/public/Script/general.js
@@ -0,0 +1,59 @@
+function GetCustommerNavBar(){
+    return '\
+        <ul>\
+          <li><p class="group-name"> Groep 3B</p></li>\
+          <li style="float:right"><a href="#logout">Logout</a></li>\
+          <li id="myProfile" style="float:right"><a href="#myProfile">My Profile</a></li>\
+          <li id="myAccounts" style="float:right"><a href="#myAccounts">My Accounts</a></li>\
+          <li id="home" style="float:right"><a href="#home">Home</a></li>\
+        </ul>'
+}
+
+function GetEmployeeNavBar(){
+    return '\
+        <ul>\
+            <li><p class="group-name"> Groep 3B</p></li>\
+            <li style="float:right"><a href="#logout">Logout</a></li>\
+            <li id="myProfile" style="float:right"><a href="#myProfile">My Profile</a></li>\
+            <li id="dashboard" style="float:right"><a href="#dashboard">Dashboard</a></li>\
+            <li id="myAccounts" style="float:right"><a href="#myAccounts">My Accounts</a></li>\
+            <li id="home"style="float:right"><a href="#home">Home</a></li>\
+        </ul>'
+}
+
+function SetNavBar(active){
+    var role = "Customer"
+    var navbar;
+    if(role == 'Customer'){
+        navbar = GetCustommerNavBar()
+    }
+    else if(role == 'Employee'){
+        navbar = GetEmployeeNavBar()
+    }
+    $("nav").html(navbar)
+    SetItemActive(active)
+}
+
+function SetItemActive(active){
+    document.getElementById(active).classList.add("active")
+}
+
+function logout() {
+    $.ajax({
+        type: "DELETE",
+        url: "http://localhost:8080/api/Logout",
+        headers: {
+            "session": sessionStorage.getItem("session")
+        },
+        complete: function(jqXHR) {
+            switch (jqXHR.status) {
+                case 200:
+                    alert("You have been logged out.");
+                    window.location.href = './Login.html';
+                    break;
+                default:
+                    alert("Oops! Something went wrong.");
+            }
+        }
+    });
+}
