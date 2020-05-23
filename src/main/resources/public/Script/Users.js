e7e5b662c0970b81bcb0bbb127d903e99270ddbf
thijsvtol <43065692+thijsvtol@users.noreply.github.com>
2020-05-23 15:04:01 +0200

    JS Cleanup

--- src/main/resources/public/Script/Users.js
+++ src/main/resources/public/Script/Users.js
@@ -1,60 +1,55 @@
+var currentUser;
+var isOwner;
+
 function GetUsers(){
-    var userId = GetUserId()
+    var SeachString = $( "input[id=seaching]" ).val()
 
-    if(userId != null){
-
-        var offset = null;
-        var limit = null;
-        var SeachString = $( "input[id=seaching]" ).val()
-
-        if(SeachString != null && SeachString != ""){
-            var header = {
-                "session": sessionStorage.getItem("session")
-            };
-            var data = {
-                "offset": 0,
-                "limit": 100,
-                "searchname": SeachString 
-            };
-        }
-        else{
-            var header = {
-                "session": sessionStorage.getItem("session")
-            };
-            var data = {
-                "searchname": SeachString
-            };
-        }
-        $.ajax({
-            type: "Get",
-            url: "http://localhost:8080/api/Users",
-            data: data,
-            headers: header,
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            complete: function(jqXHR) {
-                switch (jqXHR.status) {
-                    case 200:
-
-                        break;
-                    default:
-                        alert("Oops! Something went wrong.");
-                }
-            },
-            success: function(result){
-                MakeUser(result);
-            }
-        });
+    if(SeachString != null && SeachString != ""){
+        var header = {
+            "session": sessionStorage.getItem("session")
+        };
+        var data = {
+            "offset": 0,
+            "limit": 100,
+            "searchname": SeachString 
+        };
     }
     else{
-        alert("You are not logged in!")
-        window.location.href = './Login.html';
+        var header = {
+            "session": sessionStorage.getItem("session")
+        };
+        var data = {
+            "searchname": SeachString
+        };
     }
+    $.ajax({
+        type: "Get",
+        url: "http://localhost:8080/api/Users",
+        data: data,
+        headers: header,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        complete: function(jqXHR) {
+            switch (jqXHR.status) {
+                case 200:
+
+                    break;
+                default:
+                    alert("Oops! Something went wrong.");
+            }
+        },
+        success: function(result){
+            MakeUser(result);
+        }
+    });
 }
 
 $(document).ready(function(){
+    //Check if element excists in HTML file
+    if(document.getElementById("seaching")){
         $('#seaching').on('keyup paste',username_check);
         GetUsers();
+    }
 });
 
 function username_check(){ 
@@ -65,7 +60,7 @@ function MakeUser(users){
     $("#Users-box").empty();
     $.each(users, function(i) {
         console.log(users[i]);
-        $( "#Users-box" ).append("<a href='ViewUser.html?Id="+users[i].id+"'>"+
+        $( "#Users-box" ).append("<a href='ViewUser.html?id="+users[i].id+"'>"+
             "<div class='user-box'>"+
             "<i class='arrow right'></i>"+
             "<div class='userName'> "+
@@ -79,20 +74,184 @@ function MakeUser(users){
     }); 
 }
 
-function GetUserId(){
-    var userId = null;
+function CreateUser(){
+    var userId = GetCurrentUserId()
+
+    if(userId != null){
+        var newUser = JSON.stringify({
+            "username": $( "input[name=username]" ).val(),
+            "password": $( "input[name=password]" ).val(),
+            "firstName": $( "input[name=firstname]" ).val(),
+            "prefix": $( "input[name=prefix]" ).val(),
+            "lastName": $( "input[name=lastname]" ).val(),
+            "email": $( "input[name=email]" ).val(),
+            "birthdate": $( "input[name=birthdate]" ).val(),
+            "address": $( "input[name=address]" ).val(),
+            "postalcode": $( "input[name=postalcode]" ).val(),
+            "city": $( "input[name=city]" ).val(),
+            "phoneNumber": $( "input[name=phonenumber]" ).val(),
+            "type": $( "select[name=type]" ).val(),
+            "active": true
+        });
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:8080/api/Users",
+            data: newUser,
+            headers: {
+                "session": sessionStorage.getItem("session")
+            },
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            complete: function(jqXHR) {
+                switch (jqXHR.status) {
+                    case 201:
+                        alert("User created!");
+                        window.location.href = './RegisterAccount.html';
+                        break;
+                    default:
+                        alert (jqXHR.error);
+                        alert("Oops! Something went wrong.");
+                }
+            }
+        });
+    }
+    else{
+        alert("You are not logged in!")
+        window.location.href = './Login.html';
+    }
+}
+
+function LoadMyProfileInfo() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+
+    if (id == null) {
+        isOwner = true;
+        currentUser = GetUser(GetCurrentUserId());
+    } else {
+        isOwner = false;
+        currentUser = GetUser(id);
+    }
+
+    $("#username").val(currentUser.username);
+    $("#password").val(currentUser.password);
+    $("#firstname").val(currentUser.firstName);
+    $("#prefix").val(currentUser.prefix);
+    $("#lastname").val(currentUser.lastName);
+    $("#email").val(currentUser.email);
+    $("#birthdate").val(currentUser.birthdate);
+    $("#address").val(currentUser.address);
+    $("#city").val(currentUser.city);
+    $("#postalcode").val(currentUser.postalcode);
+    $("#phonenumber").val(currentUser.phoneNumber);
+
+    if (GetCurrentUserRole() == "Customer" || isOwner) {
+        document.getElementById("disableUserBtn").style.visibility = "hidden";
+    }
+}
+
+function disableUser() {
     $.ajax({
-        type: "GET",
-        url: "http://localhost:8080/api/SessionToken/"+sessionStorage.getItem("session"),
+        type: "PUT",
+        url: "http://localhost:8080/api/Users",
+        data: JSON.stringify({
+            active: false,
+            address: currentUser.address,
+            birthdate: currentUser.birthdate,
+            city: currentUser.city,
+            email: currentUser.email,
+            firstName: currentUser.firstName,
+            id: currentUser.id,
+            lastName: currentUser.lastName,
+            password: currentUser.password,
+            phoneNumber: currentUser.phoneNumber,
+            postalcode: currentUser.postalcode,
+            prefix: currentUser.prefix,
+            type: currentUser.type,
+            username: currentUser.username
+        }),
         headers: {
             "session": sessionStorage.getItem("session")
         },
         contentType: "application/json; charset=utf-8",
         dataType: "json",
-        async: false,
-        success: function(data){
-            userId = data.userId
+        complete: function(jqXHR) {
+            switch (jqXHR.status) {
+                case 201:
+                    alert("Update Successful.");
+                    location.reload();
+                    break;
+                default:
+                    alert("Oops! Something went wrong.");
+            }
         }
     });
-    return userId
+}
+
+function updateUser() {
+    var address = document.getElementById("address").value;
+    var birthdate = document.getElementById("birthdate").value;
+    var city = document.getElementById("city").value;
+    var email = document.getElementById("email").value;
+    var firstName = document.getElementById("firstname").value;
+    var id = currentUser.id;
+    var lastName = document.getElementById("lastname").value;
+    var password = document.getElementById("password").value;
+    var phoneNumber = document.getElementById("phonenumber").value;
+    var postalcode = document.getElementById("postalcode").value;
+    var prefix = document.getElementById("prefix").value;
+    var type = currentUser.type;
+    var username = document.getElementById("username").value;
+
+    $.ajax({
+        type: "PUT",
+        url: "http://localhost:8080/api/Users",
+        data: JSON.stringify({
+            active: true,
+            address: address,
+            birthdate: birthdate,
+            city: city,
+            email: email,
+            firstName: firstName,
+            id: id,
+            lastName: lastName,
+            password: password,
+            phoneNumber: phoneNumber,
+            postalcode: postalcode,
+            prefix: prefix,
+            type: type,
+            username: username
+        }),
+        headers: {
+            "session": sessionStorage.getItem("session")
+        },
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        complete: function(jqXHR) {
+            switch (jqXHR.status) {
+                case 201:
+                    alert("Update Successful.");
+                    break;
+                default:
+                    alert("Oops! Something went wrong.");
+            }
+        }
+    });
+}
+
+function GetUser(id) {
+    var user = null;    
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8080/api/Users/'+ id,
+        headers: { "session": sessionStorage.getItem("session") },
+        async: false,
+        success: function(result) {
+            user = result;
+        },
+        error: function(error) {
+            alert("Something went wrong! " + error);
+        }
+    });
+    return user;
 }
--- src/main/resources/public/Script/accounts.js
+++ src/main/resources/public/Script/accounts.js
@@ -1,3 +1,11 @@
+var currentIban = null;
+var currentAccount = null;
+
+function SetIban(){
+    this.currentIban = getUrlParameter("iban");
+    this.currentAccount = GetAccount(currentIban);
+}
+
 function CreateAccount(){
     var userId = GetCurrentUserId()
 
@@ -39,4 +47,204 @@ function CreateAccount(){
         alert("You are not logged in!")
         window.location.href = './Login.html';
     }
+}
+
+function GetAccountsByUserId() {
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8080/api/Users/'+GetCurrentUserId()+'/Accounts',
+        headers: {
+            "session": sessionStorage.getItem("session")
+        },
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        async: false,
+        success: function(results) {
+            console.log(results)
+            currentsOutput = "";
+            savingsOutput = "";
+
+            $.each(results, function (i, account) {
+                if (account.type == "Current") {
+                    currentsOutput += 
+                    '<a href="CustomerViewAccount.html?iban='+account.iban+'" class="clickAccount">\
+                        <div class="account">\
+                            <p class="iban">'+ account.iban +'</p>\
+                            <p class="price">'+ account.balance.balance.toFixed(2) +'</p>\
+                            <p class="currency">EUR</p>\
+                            <img src="Images/arrow_right.png" class="arrow-right-accounts">\
+                        </div>\
+                    </a>'
+                } else{
+                    savingsOutput +=
+                    '<a href="CustomerViewAccount.html?iban='+account.iban+'" class="clickAccount">\
+                        <div class="account">\
+                            <p class="iban">'+ account.iban +'</p>\
+                            <p class="price">'+ account.balance.balance.toFixed(2) +'</p>\
+                            <p class="currency">EUR</p>\
+                            <img src="Images/arrow_right.png" class="arrow-right-accounts">\
+                        </div>\
+                    </a>'
+                }
+            });
+
+            if (currentsOutput == "") {
+                currentsOutput += 
+                '<div class="noAccount">\
+                    <p class="noAccountText">You do not have any currents accounts</p>\
+                </div>'
+            }
+
+            if (savingsOutput == "") {
+                savingsOutput += 
+                '<div class="noAccount">\
+                    <p class="noAccountText">You do not have any savings accounts</p>\
+                </div>'
+            }
+            SetListOfAccounts(currentsOutput, savingsOutput)
+        },
+        error: function(error) {
+            console.log(error);     
+        }
+    });
+}
+
+function SetListOfAccounts(currentsOutput, savingsOutput){
+    $("#currentsAccounts").html(currentsOutput)
+    $("#savingsAccounts").html(savingsOutput)
+}
+
+function SetValues() {
+    $("#ibanFrom").val(currentIban);
+    $("#currencyTagSmall").text(currentAccount.currency);
+    $("#current-iban").text(currentIban);
+    $("#account-balance").text(currentAccount.balance.balance.toFixed(2));
+    $("#currency-tag").text(currentAccount.currency);
+}
+
+function GetAccount(iban) {
+    var account = null;
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8080/api/Accounts/iban/'+iban,
+        headers: { "session": sessionStorage.getItem("session")},
+        async: false,
+        success: function(result) {
+            account = result;
+        },
+        error: function(error) {
+            console.log(error);
+            
+        }
+    });
+    return account;
+}
+
+function GetAccounts(){
+    var SeachString = $( "input[id=seaching]" ).val()
+    if(SeachString != null && SeachString != ""){
+        var header = {
+            "session": sessionStorage.getItem("session")
+        };
+        var data = {
+            "offset": 0,
+            "limit": 100,
+            "iban": SeachString 
+        };
+    }
+    else{
+        header = {
+            "session": sessionStorage.getItem("session")
+        };
+        data = {
+            "iban": SeachString,
+        };
+    }
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/api/Accounts",
+        data: data,
+        headers: header,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(result){
+            console.log(result)
+            MakeUser(result);
+        },
+        error: function(){
+            alert("Oops! Something went wrong.");
+        }
+    });
+}
+
+$(document).ready(function(){
+    if(document.getElementById("seaching")){
+        $('#seaching').on('keyup paste',iban_check);
+        GetAccounts();
+    }
+});
+
+function iban_check(){ 
+    GetAccounts();
+}
+
+function MakeUser(account){
+    $("#Accounts-box").empty();
+    $.each(account, function(i) {
+        $( "#Accounts-box" ).append("<a href='EmployeeViewAccount.html?iban="+account[i].iban+"'>"+
+            "<div class='Account-box'>"+
+            "<i class='arrow right'></i>"+
+            "<div class='iban'> "+
+            account[i].iban+
+            "</div>"+
+            "<address class='typeSaving'>"+
+            account[i].type+
+            " </address>"+
+            "</div>"+
+            "</a>"
+        );
+    }); 
+}
+
+function DisableAccount(){
+    var account = GetAccount(currentIban);
+    var json = JSON.stringify({
+            id: account.id,
+            userId: account.userId,
+            type: account.type,
+            currency: account.currency,
+            balance: account.balance,
+            active: false,
+            iban: account.iban
+        })
+    console.log(json);
+
+    $.ajax({
+        type: "PUT",
+        url: "http://localhost:8080/api/Accounts",
+        data: json,
+        headers: {
+            "session": sessionStorage.getItem("session")
+        },
+        contentType: "application/json;charset=UTF-8",
+        complete: function(jqXHR) {
+            switch (jqXHR.status) {
+                case 200:
+                    alert("Account has been disabled");
+                    window.location.href = './#jessedeel.html';
+                    break;
+                default:
+                    alert("Oops! Something went wrong.");
+            }
+        }
+    });
+}
+
+function GetHeader(){
+    var header = 
+    '<h2 id="current-iban">'+currentIban+'</h2>\
+    <h2 id="account-balance">€ '+GetAccount(currentIban).balance.balance+'</h2>\
+    <p class="currency-tag">'+GetAccount(currentIban).currency+'</p>';
+    
+    $("#account-header").html(header);
 }
