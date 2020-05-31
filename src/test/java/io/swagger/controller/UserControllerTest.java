21d8aa6e11d98412ca722f0fcb60020056cbe5b8
dantim1997 <43065678+dantim1997@users.noreply.github.com>
2020-05-31 12:47:35 +0200

    alle fixes voor unit test

--- src/test/java/io/swagger/controller/UserControllerTest.java
+++ src/test/java/io/swagger/controller/UserControllerTest.java
@@ -1,5 +1,6 @@
 package io.swagger.controller;
 
+import io.swagger.dao.UserRepository;
 import io.swagger.model.Account;
 import io.swagger.model.User;
 import io.swagger.service.AccountService;
@@ -15,40 +16,53 @@ import org.springframework.data.domain.Pageable;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.mockito.BDDMockito.given;
 
 @SpringBootTest
 class UserControllerTest {
     @Autowired
-    @MockBean
     private UserService service;
-    private List<User> users;
+    private User testUser;
+    private User adrie;
 
     @BeforeEach
     public void setup() {
-        List<User> users = Arrays.asList(
-                new User("Inholland-Bank", "Welcome567?", "Bank", "", "Inholland", "bank@inholland-bank.nl",
-                        "2019/1/1", "Arnold straat 33", "1354PK", "Haarlem", "0638313905", User.TypeEnum.BANK, true),
-                new User("Adrie538", "Welkom123!", "Andries", "", "Komen", "AndriesK@gmail.com",
-                        "2019/1/1", "Bloemendotter 12", "1958TX", "Haarlem", "0637291827", User.TypeEnum.EMPLOYEE, true),
-                new User("SjaakMaster", "Test123!", "Sjaak", "Van", "Bergen", "SjaakVBergen@gmail.com",
-                        "2019/1/1", "Jacobstraat", "1938DR", "Amsterdam", "0638273745", User.TypeEnum.CUSTOMER, true)
-        );
+        testUser = new User("tester123", "Test123!", "test", "Van", "test", "test@gmail.com",
+                "2019/1/1", "test", "1938DR", "Amsterdam", "0638273745", User.TypeEnum.CUSTOMER, true);
+        adrie = new User("Adrie538", "Welkom123!", "Andries", "", "Komen", "AndriesK@gmail.com",
+                "2019/1/1", "Bloemendotter 12", "1958TX", "Haarlem", "0637291827", User.TypeEnum.EMPLOYEE, true);
+        adrie.setId(2L);
+    }
+
+    @Test
+    public void createAnUser(){
+        service.createUser(testUser);
+        assertEquals(service.getUserByUsername("tester123"), testUser);
     }
 
     @Test
     public void GetAllUsers() throws Exception {
-        given(service.getAllUsers()).willReturn(users);
+        List<User> users = service.getAllUsers();
+        assertNotEquals(users.size(), 0);
     }
 
     @Test
     public void GetUserByIdTwo() throws Exception {
-        given(service.getUserById((long)2)).willReturn(users.get(1));
+        User test = service.getUserById((long)2);
+        assertEquals(service.getUserById((long)2), adrie);
     }
 
     @Test
     public void GetUserByLastname() throws Exception {
         Pageable pageable = PageRequest.of(0, 100);
-        given(service.getAllUsersByLastname("Komen", pageable).get(0)).willReturn(users.get(1));
+        assertNotEquals(service.getAllUsersByLastname("Komen", pageable).size(), 0);
+    }
+
+    @Test
+    public void GetUserByUsername() throws Exception {
+        Pageable pageable = PageRequest.of(0, 100);
+        assertNotEquals(service.getAllUsersByUsername("Adrie538", pageable).size(), 0);
     }
 }
